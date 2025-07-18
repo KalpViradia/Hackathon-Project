240f0885eaedc349eacf20b241f29e136535eaf3
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2025-07-18 18:54:49 +0530

    Delete Backend directory

--- Backend/routes/notification.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-const auth = require('../middleware/auth');
-
-router.get('/', auth, notificationController.getNotifications);
-router.put('/', auth, notificationController.markAllAsRead);
-
-module.exports = router; 
