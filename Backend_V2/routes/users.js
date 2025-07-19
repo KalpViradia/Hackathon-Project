582980c6c6f7c324b620cbc93f157d3b8276e543
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2025-07-19 15:04:04 +0530

    Update users.js

--- Backend_V2/routes/users.js
+++ Backend_V2/routes/users.js
@@ -7,6 +7,14 @@ const { body } = require('express-validator');
 // Get current user
 router.get('/me', auth, userController.getMe);
 
+// Follow/Unfollow routes
+router.post('/follow/:id', auth, userController.followUser);
+router.get('/followers', auth, userController.getFollowers);
+router.get('/following', auth, userController.getFollowing);
+
+// Block/Unblock user
+router.post('/block/:id', auth, userController.blockUser);
+
 // Get user by ID
 router.get('/:id', auth, userController.getUserById);
 
@@ -16,13 +24,5 @@ router.put('/me', [
   body('bio').optional().trim().isLength({ max: 500 }),
   body('avatar').optional().isURL()
 ], userController.updateProfile);
-
-// Follow/Unfollow routes
-router.post('/follow/:id', auth, userController.followUser);
-router.get('/followers', auth, userController.getFollowers);
-router.get('/following', auth, userController.getFollowing);
-
-// Block/Unblock user
-router.post('/block/:id', auth, userController.blockUser);
 
 module.exports = router;
